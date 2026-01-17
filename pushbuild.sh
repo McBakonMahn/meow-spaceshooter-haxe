ea47eac364e2379bad18b9c8a06dc886c01fedb2
McBakonMahn <ragemasterweak@yahoo.com>
2026-01-17 10:10:26 +0200

    updated gitignore

--- pushbuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git add -f export/html5/bin
-git commit -m "Build update: $(date)"
-git push origin `git subtree split --prefix export/html5/bin source`:refs/heads/htmlbin --force
-git reset --soft HEAD~1
-git rm -r --cached export/html5/bin
